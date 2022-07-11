18bc14205cbe690a449df8f1866ace27f5d6d797
xstartstaticx <daniel.reiller@icloud.com>
2022-07-11 13:09:13 +0200

    initial commit

--- solution.js
+++ solution.js
@@ -1,1 +1,49 @@
+// 1
+const euroCities = ["Hamburg", "Granada", "Kiev", "Dublin", "Prague"];
+console.log("Q1a:", euroCities);
+const secondCity = euroCities[1];
+console.log("Q1b:", secondCity);
 
+// 2
+euroCities.splice(0, 1, "Berlin");
+console.log("Q2:", euroCities);
+
+// 3
+console.log("Q3:", euroCities.length);
+
+// 4
+euroCities.pop();
+console.log("Q4:", euroCities);
+
+// 5
+euroCities.push("Budapest");
+console.log("Q5:", euroCities);
+
+// 6
+const asianCities = [
+  "Singapore",
+  "Kyoto",
+  "Seoul",
+  "Hong Kong",
+  "Wuhan",
+  "Dubai",
+  "Shenzhen",
+];
+console.log("Q6:", asianCities);
+
+// 7
+const euroJoined = euroCities.join();
+const asianJoined = asianCities.join();
+const worldCities = (euroJoined + "," + asianJoined).split(",");
+console.log("Q7:", worldCities);
+
+// 8
+console.log("Q8:", worldCities.reverse());
+// Extra
+// console.log("Qb:", (euroJoined + "," + asianJoined).split(",").reverse());
+
+// 9 Bonus
+
+// 10 Bonus
+
+// 11 Bonus
